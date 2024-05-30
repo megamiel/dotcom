8cf3e7ee6c1661b143824534ebf9027c495b1e0d
megamiel <149140489+megamiel@users.noreply.github.com>
2024-05-30 19:41:27 +0900

    Update loanList.js

--- scripts/loanList.js
+++ scripts/loanList.js
@@ -24,8 +24,8 @@ document.getElementById("loanButton").addEventListener("click", () => {
     var numStr = ['令和5年秋期', '令和5年春期', '令和4年秋期', '令和4年春期', '令和3年秋期', '令和3年春期', '令和2年秋期', '令和元年秋期', '平成31年春期', '平成30年秋期', '平成30年春期', '平成29年秋期', '平成29年春期', '平成28年秋期', '平成28年春期', '平成27年秋期', '平成27年春期', '平成26年秋期', '平成26年春期', '平成25年秋期', '平成25年春期', '平成24年秋期', '平成24年春期', '平成23年秋期'];
     numStr=numStr.reverse();
     var subsStr = ['情報セキュリティ', '経営戦略', 'データベース', 'サービスマネジメント', 'プロジェクトマネジメント', '組込みシステム開発'];
-    const todayExam = Math.floor(diffDays / 3);
-    const todayFinalSubject = diffDays % 3 * 2 + 1;
+    const todayExam = Math.floor((diffDays-1) / 6);
+    const todayFinalSubject = (diffDays-1)%6;
     var array = [];
     for (var i = 0; i <= todayExam; i++) {
         for (var j = 0; j < subsStr.length; j++) {
